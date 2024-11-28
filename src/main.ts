5fe6bc1316c033c62da8cf883e33d7e63eea6617
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-11-28 14:01:40 -0600

    converted codebase to typescript

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,33 @@
 // Capture Elements
-var themeBtn = document.getElementById("themeBtn");
+const themeBtn = document.getElementById("themeBtn");
+
+
+
 // --------------------------------------------------------------------------------------- \\
+
 // Initial Theme
-var theme = localStorage.getItem("theme") || "dark";
+let theme = localStorage.getItem("theme") || "dark";
 localStorage.setItem("theme", theme);
+
 // Function to apply theme + Invoke 
-function applyTheme() {
+function applyTheme(){
     document.documentElement.setAttribute("data-theme", theme);
-}
-;
+};
 applyTheme();
+
 // Theme toggle event listener
-themeBtn.addEventListener("click", function () {
-    if (localStorage.getItem("theme") === "dark") {
+themeBtn.addEventListener("click", ()=>{
+    
+    if(localStorage.getItem("theme") === "dark"){
         theme = "light";
-    }
-    else {
+    }else{
         theme = "dark";
     }
+
     applyTheme();
     localStorage.setItem("theme", theme);
+
     console.log("clicked!");
 });
+
 // Password Generation Logic
-//# sourceMappingURL=main.js.map
