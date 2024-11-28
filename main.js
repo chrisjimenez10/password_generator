c25f321c8769f4c66ebd43ff16f4879e37d9eaa1
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-11-27 23:48:09 -0600

    initial commit with basic layout with theme toggle feature

---
+++ main.js
@@ -0,0 +1,32 @@
+// Capture Elements
+const themeBtn = document.getElementById("themeBtn");
+
+
+
+// --------------------------------------------------------------------------------------- \\
+
+// Initial Theme
+let theme = localStorage.getItem("theme") || "dark";
+localStorage.setItem("theme", theme);
+
+// Function to apply theme + Invoke 
+function applyTheme(){
+    document.documentElement.setAttribute("data-theme", theme);
+    themeBtn.style.background = "rgba(235,235,235, 75%)";
+};
+applyTheme();
+
+// Theme toggle event listener
+themeBtn.addEventListener("click", ()=>{
+    
+    if(localStorage.getItem("theme") === "dark"){
+        theme = "light";
+    }else{
+        theme = "dark";
+    }
+
+    applyTheme();
+    localStorage.setItem("theme", theme);
+
+    console.log("clicked!");
+});
